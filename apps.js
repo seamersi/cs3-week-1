d37d45b46a02201707c256c6ad8d23187b247447
seamersi <101748100+seamersi@users.noreply.github.com>
2022-08-25 07:50:41 -0500

    Add files via upload

--- apps.js
+++ apps.js
@@ -23,3 +23,74 @@ function  sentences(verb, adverb, person, place, adj){
     return sentenceArray[ranN]; 
     
 }
+// game 2 
+
+var a = [1, 2, 4, 9]
+
+var wordArr = [
+    {
+        word: "dog",
+        hint: "Company you keep at home"
+    },
+    {
+        word: "place",
+        hint: "Somewhere at any time"
+    },
+    {
+        word: "tree",
+        hint: "it grows from the ground"
+    },
+    {
+        word:"computer",
+        hint:"You're using it right now"
+
+    }
+]
+
+var rN = Math.floor(Math.random() * wordArr.length);
+
+var word = wordArr[rN].word;
+var hint = wordArr[rN].hint;
+var correct = []
+var counter = word.length + 2
+
+document.getElementById("word").innerHTML = word;
+document.getElementById("hint").innerHTML = hint;
+document.getElementById("wrong").innerHTML = counter;
+
+var splitWord = word.split("");
+
+var emptySplitword = [];
+
+console.log(splitWord)
+
+for (i = 0; i < splitWord.length; i++) {
+    emptySplitword[i] = "-"
+    console.log(emptySplitword);
+    document.getElementById("word").innerHTML = emptySplitword.join(" ")
+}
+
+function enterLetter() {
+    var length = document.getElementById("letter").value
+
+    for( x = 0; x < splitWord.length; x++) {
+        if(letter == splitWord[x]) {
+            emptySplitword[x] = letter;
+            w.innerHTML = emptySplitword.join(" ");
+            document.getElementById("letter").value = "";
+            correct.push(1)
+        }
+        else{
+            document.getElementById("letter").value = "";
+        }
+    }
+
+    if(correct.length == emptySplitword.length){
+        alert("THE END")
+    }else if(counter > 1){
+        counter -= 1;
+        document.getElementById("wrong").innerHTML = counter
+    } else if(counter == 1) {
+        alert("you lost")   
+    }
+}
